2a49358f89f99170db1990a7e8edd14b9ba66a22
unknown <d.flores.3235@institutonacional.cl>
2018-03-21 20:35:55 -0300

    Mi primer commit

---
+++ suma.py
@@ -0,0 +1,8 @@
+Python 2.7.12 (v2.7.12:d33e0cf91556, Jun 27 2016, 15:19:22) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> def suma(a,b):
+	return a+b
+
+>>> suma(4,1)
+5
+>>> 
